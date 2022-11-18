5acbcb256d3c5e8bd002cd737c5539ebdcd96aca
A24-N <117143210+A24-N@users.noreply.github.com>
2022-11-18 05:16:34 +0000

    [start]task3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def show
     @book = Book.find(params[:id])
     @nbook = Book.new
@@ -23,9 +25,6 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    if @book.user_id != current_user.id
-      redirect_to books_path
-    end
   end
 
   def update
@@ -47,5 +46,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
